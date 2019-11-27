17ef4f8b9d98dad749d2be6d547075d719ee7ac8
ajdm432 <alexandermountain@gmail.com>
2019-11-26 21:07:23 -0800

    Finish credits and year predicate. Add NLP

--- cogsplanner.pl
+++ cogsplanner.pl
@@ -64,84 +64,83 @@ askQuestion(List) :-
 	read(Question), % not sure about syntax here! TODO 
 	ask(Question, _),
 
+q(Ans) :-
+    write("Ask me: "), flush_output(current_output),
+    readln(Ln),
+    ask(Ln,Ans).
+
 
 	
 % NATURAL LANGUAGE PARSER
 
+% noun_phrase(L0,L4,Entity) is true if
+%  L0 and L4 are list of words, such that
+%        L4 is an ending of L0
+%        the words in L0 before L4 (written L0-L4) form a noun phrase
+%  Entity is an individual that the noun phrase is referring to
 
-% question(Question,QR,Object) is true if Query provides an answer about Object to Question
-question(['Is' | T0],T2,Obj,St) :-
-    noun_phrase(T0,T1,Obj,St),
-    mp(T1,T2,Obj,St).
-question(['What',is | T0], T1, Obj,St) :-
-    mp(T0,T1,Obj,St).
-question(['What',is | T0],T1,Obj,St) :-
-    noun_phrase(T0,T1,Obj,St).
-question(['What' | T0],T2,Obj,St) :-
-    noun_phrase(T0,T1,Obj,St),
-    mp(T1,T2,Obj,St).
-
-% ask(Q,A) gives answer A to question Q. Is true if A is the answer to question Q. 
-ask(Q,A,St) :-
-    question(Q,[],A,St).
-
-
-% States
 % A noun phrase is a determiner followed by adjectives followed
 % by a noun followed by an optional modifying phrase:
-noun_phrase(T0,T4,Ind,St) :-
-    det(T0,T1,Ind),
-    adjectives(T1,T2,Ind,St),
-    noun(T2,T3,Ind,St),
-    mp(T3,T4,Ind,St).
+noun_phrase(L0,L4,Entity) :-
+    det(L0,L1,Entity),
+    adjectives(L1,L2,Entity),
+    noun(L2,L3,Entity),
+    mp(L3,L4,Entity).
 
 % Determiners (articles) are ignored in this oversimplified example.
 % They do not provide any extra constraints.
-det([the | T],T,_).
-det([a | T],T,_).
-det([an | T], T, _).
-det([are | T], T, _).
-det(T,T,_).
+det([the | L],L,_).
+det([a | L],L,_).
+det(L,L,_).
 
-% adjectives(T0,T1,Ind) is true if 
-% T0-T1 is an adjective is true of Ind
-adjectives(T0,T2,Ind,St) :-
-    adj(T0,T1,Ind,St),
-    adjectives(T1,T2,Ind,St).
-adjectives(T,T,_,_).
+% adjectives(L0,L1,Entity) is true if 
+% L0-L1 is a sequence of adjectives that true of Entity
+adjectives(L0,L2,Entity) :-
+    adj(L0,L1,Entity),
+    adjectives(L1,L2,Entity).
+adjectives(L,L,_).
 
 % An optional modifying phrase / relative clause is either
 % a relation (verb or preposition) followed by a noun_phrase or
 % 'that' followed by a relation then a noun_phrase or
 % nothing 
-mp(T0,T2,Subject,St) :-
-    reln(T0,T1,Subject,Object,St),
-    noun_phrase(T1,T2,Object,St).
-mp([that|T0],T2,Subject,St) :-
-    reln(T0,T1,Subject,Object,St),
-    noun_phrase(T1,T2,Object,St).
-mp(T,T,_,_).
+mp(L0,L2,Subject) :-
+    reln(L0,L1,Subject,Object),
+    noun_phrase(L1,L2,Object).
+mp([that|L0],L2,Subject) :-
+    reln(L0,L1,Subject,Object),
+    noun_phrase(L1,L2,Object).
+mp(L,L,_).
 
-% DICTIONARY
+% adj(L0,L1,Entity) is true if L0-L1 
+% is an adjective that is true of Entity
 
-adj([faculty, of, Fac | T], T, Obj,_) :- faculty(Obj, Fac).
-adj([Fac | T], T, Obj, _) :- faculty(Obj, Fac).
+adj([module | L], L, Enitity):- isModule(Entity).
+adj([core | L], L, Enitity):- core(Entity).
 
-noun([eligible, course | T], T, Obj, St) :- isEligible(Obj, St).
-noun([my, eligible, courses | T], T, Obj, St) :- isEligible(Obj, St).
-noun([course, can, i, take | T], T, Obj, St) :- isEligible(Obj, St).
+noun([credits | L], L, Entity):- credits(Entity, N).
+% Do we really need a 'degree' noun? noun([degree | L], L, Entity):-
+noun([course | L], L, course(X, Y)).
+noun([courses | L], L, course(X, Y)). %not sure about this one.
+noun([pre-requisites | L], L, Entity):- (isEligible(Entity, X)).
+noun([faculty | L], L, Entity):- course(Entity, _).
 
-noun([course, Department, Number | T],T,course(Department, Number),_) :- course(Department,Number).
-noun([module, course, Department, Number | T], T, course(Department, Number), _) :- isModule(course(Department, Number).
-noun([course | T], T, course(Department, Number),_) :- course(Department,Number).
-noun([module, course | T], T, Obj,_) :- isModule(Obj).
-noun([my, possible, courses | T], T, Obj, []) :- noReqs(Obj).
-noun([possible, courses | T], T, Obj, St) :- requires(Obj, ListCourses), member(Y, St), member(Y, ListCourses).
-noun([my, possible, courses | T], T, Obj, St) :- requires(Obj, ListCourses), member(Y, St), member(Y, ListCourses).
-noun([course, i, have, taken | T], T, Obj, St) :- member(Obj, St).
+reln([requirments| L],L,course(X,Y),course(A,B)) :- requires(course(X,Y),course(A,B)).
 
-reln([required, for | T], T, Obj, Course,_) :- requires(Course, Obj).
-reln([requires | T], T, Obj, Course,_) :- requires(Course, Obj).
+% question(Question,QR,Entity) is true if Query provides an answer about Entity to Question
+question(['What',are | L0], L1, Entity) :-
+    mp(L0,L1,Entity).
+question(['What',are | L0],L1,Entity) :-
+    noun_phrase(L0,L1,Entity).
+question(['What' | L0],L2,Entity) :-
+    noun_phrase(L0,L1,Entity),
+    mp(L1,L2,Entity).
+question(['How',many | L0],L2,Entity) :-
+    noun_phrase(L0,L1,Entity).
+question(['How',many | L0],L2,Entity) :-
+    noun_phrase(L0,L1,Entity),
+    mp(L1,L2,Entity).
 
-
-	
+% ask(Q,A) gives answer A to question Q
+ask(Q,A) :-
+    question(Q,[],A).
